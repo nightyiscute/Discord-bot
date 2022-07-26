29fe41b06a44542d6fdc1c947b8189d32d6c1bd1
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-26 17:35:07 +0800

    Update dc bot.py

--- dc bot.py
+++ dc bot.py
@@ -1,5 +1,7 @@
+import imp
 import discord
 import json
+import random
 from discord.ext import commands
 
 with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
@@ -14,13 +16,13 @@ async def on_ready(): #啟動on_ready函數
 
 @bot.command()
 async def ping(ctx): #啟動ping函數
-    await ctx.send(f'{round(bot.latency*1000)}'"(ms)")
+    await ctx.send(f'{round(bot.latency*1000)}'"(ms)") #發送延遲毫秒數
     
 @bot.command()    
-async def photo(ctx):
-    photo=discord.File(jdata['photo_neko_miku'])
-    await ctx.send(file=photo)
-
+async def photo(ctx): #啟動照片函數(photo是名字)
+    random_photo=random.choice (jdata['photo']) #定義ramdom_photo
+    photo=discord.File(random_photo) #定義photo=discord的檔案
+    await ctx.send (File=photo) #發送檔案
 
 bot.run(jdata['token'])
 
