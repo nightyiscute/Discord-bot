ae0a8cc4bccc909720a6937d47cfb09fb33b5152
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-27 15:59:12 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -7,8 +7,8 @@ from core.classes import Cog_Extantion
 
 class Main(Cog_Extantion):
     @commands.command()
-    async def ping(seif,ctx): #啟動ping函數
-        await ctx.send(f'{round(seif.bot.latency*1000)}'"(ms)") #發送延遲毫秒數
+    async def ping(self,ctx): #啟動ping函數
+        await ctx.send(f'{round(self.bot.latency*1000)}'"(ms)") #發送延遲毫秒數
 def setup(bot):
     bot.add_cog(Main(bot))
 
