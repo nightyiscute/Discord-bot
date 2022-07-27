167ebd0d6a667b17ff155245acd9fa31f8ea52bd
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-27 14:47:24 +0800

    Update dc bot.py

--- dc bot.py
+++ dc bot.py
@@ -1,14 +1,18 @@
+from ast import main
 from email import message
 import imp
 from lib2to3.pgen2 import token
+from unicodedata import name
 import discord
 import json
 import random
 from pixivpy3 import *
 from discord.ext import commands
+import os
 
 with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
     jdata=json.load(jfile)
+
 bot=commands.Bot(command_prefix='!')
 REFRESH_TOKEN=token
 cilent=discord.Client()
@@ -19,8 +23,11 @@ async def on_ready(): #啟動on_ready函數
     await channel.send('機器人上線啦!') #發送訊息
     print("bot上線拉") #在終端發送訊息
 
+for Filename in os.listdir('./cmds'):
+    if Filename.endswith('.py'):
+        bot.load_extension(f'cmds.{Filename[:-3]}')
 
 
-    
-bot.run(jdata['token'])
+if __name__=="__main__":
+    bot.run(jdata['token'])
 
