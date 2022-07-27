8bf1c8e261ee5570d7d3d0b4ffb4acfd85c21209
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-27 15:59:16 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -10,14 +10,14 @@ with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,
 class react(Cog_Extantion):
 
     @commands.command()    
-    async def photo(seif,ctx): #啟動照片函數(photo是名字)
-    random_photo=random.choice (jdata['photo']) #定義ramdom_photo
-    photo=discord.File(random_photo) #定義photo=discord的檔案
-    await ctx.send (file=photo) #發送檔案
+    async def photo(self,ctx): #啟動照片函數(photo是名字)
+        random_photo=random.choice (jdata['photo']) #定義ramdom_photo
+        photo=discord.File(random_photo) #定義photo=discord的檔案
+        await ctx.send(file=photo) #發送檔案
 
     @commands.command()    
-    async def web_photo(seif,ctx): #啟動照片函數(wed_photo是名字)
-    random_photo=random.choice (jdata['url_photo']) #定義ramdom_photo
-    await ctx.send(random_photo)
+    async def web_photo(self,ctx): #啟動照片函數(wed_photo是名字)
+        random_photo=random.choice (jdata['url_photo']) #定義ramdom_photo
+        await ctx.send(random_photo)
 def setup(bot):
     bot.add_cog(react(bot))
