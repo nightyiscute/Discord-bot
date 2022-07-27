428c22e882c06309966abb440119b86d0b5cf525
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-27 14:12:47 +0800

    Update dc bot.py

--- dc bot.py
+++ dc bot.py
@@ -1,3 +1,4 @@
+from email import message
 import imp
 from lib2to3.pgen2 import token
 import discord
@@ -10,6 +11,7 @@ with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,
     jdata=json.load(jfile)
 bot=commands.Bot(command_prefix='!')
 REFRESH_TOKEN=token
+cilent=discord.Client()
 
 @bot.event #觸發事件
 async def on_ready(): #啟動on_ready函數
@@ -17,10 +19,7 @@ async def on_ready(): #啟動on_ready函數
     await channel.send('機器人上線啦!') #發送訊息
     print("bot上線拉") #在終端發送訊息
 
-@bot.command()
-async def ping(ctx): #啟動ping函數
-    await ctx.send(f'{round(bot.latency*1000)}'"(ms)") #發送延遲毫秒數
-    
+
 @bot.command()    
 async def photo(ctx): #啟動照片函數(photo是名字)
     random_photo=random.choice (jdata['photo']) #定義ramdom_photo
@@ -32,10 +31,6 @@ async def web_photo(ctx): #啟動照片函數(wed_photo是名字)
     random_photo=random.choice (jdata['url_photo']) #定義ramdom_photo
     await ctx.send(random_photo)
 
-@bot.command()
-async def ask(ctx):
-    await ctx.send("!ping可偵測延遲 \n!photo可隨機發送圖片 \n!web_photo可以發送網址 \n!ask以查看命令列表")
-
-
+    
 bot.run(jdata['token'])
 
