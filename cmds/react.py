76405c0e725e5ae382f023bad20e159cd39c51f2
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-27 14:22:41 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -1,5 +1,23 @@
 import imp
 import discord
 from discord.ext import commands
+from core.classes import Cog_Extantion
+import json
+import random
 
-class 
+with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
+    jdata=json.load(jfile)
+class react(Cog_Extantion):
+
+    @commands.command()    
+    async def photo(seif,ctx): #啟動照片函數(photo是名字)
+    random_photo=random.choice (jdata['photo']) #定義ramdom_photo
+    photo=discord.File(random_photo) #定義photo=discord的檔案
+    await ctx.send (file=photo) #發送檔案
+
+    @commands.command()    
+    async def web_photo(seif,ctx): #啟動照片函數(wed_photo是名字)
+    random_photo=random.choice (jdata['url_photo']) #定義ramdom_photo
+    await ctx.send(random_photo)
+def setup(bot):
+    bot.add_cog(react(bot))
