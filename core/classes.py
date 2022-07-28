76cb71b41be066e33ede7b08099f6bdbf3e49cc7
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-28 19:03:09 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -4,4 +4,6 @@ from discord.ext import commands
 from core.classes import Cog_Extantion
 
 class event(Cog_Extantion):
-
+class Cog_Extantion(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
