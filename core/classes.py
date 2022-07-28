a365059bd6126e7246ad69ac95123312c9ba980f
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-28 18:44:32 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -1,7 +1,7 @@
+from lib2to3.pgen2.grammar import opmap
 import discord
 from discord.ext import commands
+from core.classes import Cog_Extantion
 
+class event(Cog_Extantion):
 
-class Cog_Extantion(commands.Cog):
-    def __init__(self,bot):
-        self.bot=bot
