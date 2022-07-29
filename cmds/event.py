03f1d1d39349f035e2a46ff382921879cbd0efa1
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-07-29 22:43:20 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -1,3 +1,5 @@
+from email import message
+from http import client
 import json
 import discord
 from discord.ext import commands
@@ -7,8 +9,17 @@ from core.classes import Cog_Extantion
 class event(Cog_Extantion): 
     @commands.Cog.listener()
     async def on_message(self,msg):
-        if msg.content=='hi':
-            await msg.channel.send('hi 你好 你很可愛')
+       
+        if msg.content.startwith('n') and msg.author != self.bot.user:
+            tmp=message.content.split("",2)
+            if len(tmp)==1:
+                await msg.channel.send('hi 你好 你很可愛')
+            else:
+                await msg.channel.send("https://nhentai.net/g/tmp[1]/")
+
+
+        
+
 
 
 
