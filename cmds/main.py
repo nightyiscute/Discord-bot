c50301dd00be70c5d6b23cdbbdc8925f1210e96a
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-08-03 22:19:00 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,3 +1,4 @@
+from email import message
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extantion
@@ -6,6 +7,10 @@ class Main(Cog_Extantion):
     @commands.command()
     async def ping(self,ctx): #啟動ping函數
         await ctx.send(f'{round(self.bot.latency*1000)}'"(ms)") #發送延遲毫秒數
+    @commands.command()
+    async def say(self,ctx,*,msg):
+        await ctx.send(msg)
+        await ctx.message.delete()
 def setup(bot):
     bot.add_cog(Main(bot))
 
