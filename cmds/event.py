634a31134deb9bbe03271efc27a7ff127ab61143
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-08-03 16:40:18 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -1,7 +1,13 @@
+import imp
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extantion
+import random 
+import json
 
+with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
+    jdata=json.load(jfile)
+    
 class event(Cog_Extantion):
     @commands.Cog.listener()
     async def on_message(self,msg):       
@@ -10,7 +16,8 @@ class event(Cog_Extantion):
         if msg.content.startswith('n'): 
             await msg.channel.send(f'https://nhentai.net/g/{msg.content[1:]}/')
         if msg.content=="hi":
-            await msg.channel.send(f"Hi Hi你好{msg.author.display_name}")
+            random_talk=random.choice(jdata['talk'])
+            await msg.channel.send(random_talk)
         if self.bot.user in msg.mentions:
             await msg.channel.send(f"tag三小{msg.author.display_name}")
         if msg.content.startswith('禁漫'):
