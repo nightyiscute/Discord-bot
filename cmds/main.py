193a44e6f2c17aced3ccc1e95140c02eea30f204
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-08-05 13:30:42 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -18,13 +18,12 @@ class Main(Cog_Extantion):
     async def my(self,ctx):
         embed=discord.Embed(title=f"{ctx.author.display_name}", url="https://youtu.be/dQw4w9WgXcQ", description="好康的", color=0x00ff6e,timestamp=datetime.datetime.now())
         embed.set_author(name="Information")
-        embed.add_field(name="11", value="1", inline=False)
+        embed.add_field(name="Never gonna give you up", value="Never gonna run around and desert you", inline=False)
         embed.set_thumbnail(url=ctx.author.avatar_url)
-        embed.add_field(name="22", value="2", inline=False)
         embed.set_footer(text=f"{ctx.author.display_name}寫於")
         await ctx.send(embed=embed)
 
-
+    @
 def setup(bot):
     bot.add_cog(Main(bot))
 
