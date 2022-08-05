214f5104dd525214b7297f946209a82a37e7619f
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-08-05 13:51:00 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,8 +1,13 @@
+import imp
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extantion
 import datetime
+import random
+import json
 
+with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
+    jdata=json.load(jfile)
 
 class Main(Cog_Extantion):
     @commands.command()
@@ -23,7 +28,31 @@ class Main(Cog_Extantion):
         embed.set_footer(text=f"{ctx.author.display_name}寫於")
         await ctx.send(embed=embed)
 
-    @
+    @commands.command()
+    async def luck(self,ctx):
+        random_luck=random.choice(jdata["luck"])
+        if random_luck=="大吉":
+            color=0x00ff1e
+            word="恭喜!今天適合買大樂透"
+        if random_luck=="吉":
+            color=0x00e1ff
+            word="恭喜!今天會有小確幸"
+        if random_luck=="中":
+            color=0xfbff00
+            word="今天運氣不好也不壞，是個平常的一天呢"
+        if random_luck=="兇":
+            color=0xff8800
+            word="今天走路要看路喔，小心踩到狗屎"
+        if random_luck=="大凶":
+            color=0xff0000
+            word=""
+        embed=discord.Embed(title=random_luck, description="好康的", color=color,timestamp=datetime.datetime.now())
+        embed.set_author(name="今日運氣")
+        embed.add_field(name=word, inline=False)
+        embed.set_footer(text=f"{ctx.author.display_name}占於")
+        
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Main(bot))
 
