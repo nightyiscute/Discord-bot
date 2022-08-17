6365d946844144e64ec553789b44946b764e35da
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2022-08-17 23:16:56 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -8,19 +8,36 @@ class task(Cog_Extantion):
     def __init__(self,*args,**kwargs): #格式化class time
         super().__init__(*args,**kwargs) #把classes 的class引用回來
 
-        async def interval():
+        async def time_task():
             await self.bot.wait_until_ready()
             self.channel=self.bot.get_channel(999993244425392231)
             while not self.bot.is_closed():
-                await self.channel.send("owo")
-                await asyncio.sleep(5) #單位為秒
-        self.bg_task=self.bot.loop.create_task(interval())
+                now_time=datetime.datetime.now
+                with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
+                    jdata=json.load(jfile)
+                if now_time==jdata['time']:
+                    await self.channel.send('working!')
+                    await asyncio.sleep(3) #單位為秒
+                else:
+                    await asyncio.sleep(1) #單位為秒
+                    pass    
+
+               # await self.channel.send("owo")
+                
+        self.bg_task=self.bot.loop.create_task(time_task())
 
     @commands.command()
     async def set_channel(self,ctx,ch:int):
         self.channel=self.bot.get_channel(ch)
         await ctx.send(f'設定頻道{self.channel.mention}')
-
+    
+    @commands.command()
+    async def set_time(self,ctx,time):
+        with open('setting.json',mode='r',encoding='utf8')as jfile: #打開setting.json,模式是read,命名為jfile
+            jdata=json.load(jfile)
+        jdata["time"]=time
+        with open('setting.json',mode='w',encoding='utf8')as jfile: #打開setting.json,模式是write,命名為jfile
+            json.dump(jdata,jfile,indent=4) #indent是縮排
 
 
 
